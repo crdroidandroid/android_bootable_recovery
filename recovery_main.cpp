0b0e00be5402167a6d2b04159148012122042a03
Ethan Yonker <dees_troy@teamw.in>
2021-01-03 23:47:09 -0800

    recovery: add support for changing slots
    * reference: https://github.com/omnirom/android_bootable_recovery/commit/1b190166eb1295c6339f6100e4fbb92c81b81ea6
    
    Change-Id: I848e003ca569dfaa572ce7641775f0ea9f2b5513
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -490,6 +490,10 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::ENTER_RESCUE);
   }
 
+  if (!android::base::GetBoolProperty("ro.build.ab_update", false)) {
+    device->RemoveMenuItemForAction(Device::SWAP_SLOT);
+  }
+
   ui->SetBackground(RecoveryUI::NONE);
   if (show_text) ui->ShowText(true);
 
