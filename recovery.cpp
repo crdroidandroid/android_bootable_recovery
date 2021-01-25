9a3f2eb266f2c7b500ee4cee5735bdcd84ac1671
Anay Wadhera <awadhera@berkeley.edu>
2021-01-25 12:46:05 -0800

    recovery: display current filesystem when going to format
    Change-Id: I98dfde91c069751144842943ad2af1f6f0c02bb1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- recovery.cpp
+++ recovery.cpp
@@ -68,6 +68,8 @@
 using android::volmgr::VolumeManager;
 using android::volmgr::VolumeInfo;
 
+using android::fs_mgr::Fstab;
+
 static constexpr const char* COMMAND_FILE = "/cache/recovery/command";
 static constexpr const char* LAST_KMSG_FILE = "/cache/recovery/last_kmsg";
 static constexpr const char* LAST_LOG_FILE = "/cache/recovery/last_log";
@@ -191,9 +193,15 @@ bool ask_to_continue_downgrade(Device* device) {
 }
 
 std::string get_preferred_fs(Device* device) {
+  Fstab fstab;
+  auto read_fstab = ReadFstabFromFile("/etc/fstab", &fstab);
   std::vector<std::string> headers{ "Choose what filesystem you want to use on /data", "Entries here are supported by your device." };
-  std::vector<std::string> items = get_data_fs_items();
   std::string fs = volume_for_mount_point("/data")->fs_type;
+  if (read_fstab) {
+      std::string current_filesystem = android::fs_mgr::GetEntryForPath(&fstab, "/data")->fs_type;
+      headers.emplace_back("Current filesystem: " + current_filesystem);
+  }
+  std::vector<std::string> items = get_data_fs_items();
 
   if (items.size() > 1) {
       size_t chosen_item = device->GetUI()->ShowMenu(
