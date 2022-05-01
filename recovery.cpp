6212b14d90352e7848b18aedae45da8e295d3f2c
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-01 15:43:05 +0800

    recovery: Do not print switch slot info without actually switching a slot
    Test: Advanced - Switch slot - Back home, no more info like "Switched slot to ."
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- recovery.cpp
+++ recovery.cpp
@@ -214,10 +214,10 @@ int set_slot(Device* device) {
   } else {
       auto result = module->setActiveBootSlot(sslot, cb);
     if (result.isOk() && ret.success) {
-      device->GetUI()->Print("Switched slot to %s.\n", slot.c_str());
+      if (slot == "A" || slot == "B") device->GetUI()->Print("Switched slot to %s.\n", slot.c_str());
       device->GoHome();
     } else {
-      device->GetUI()->Print("Error changing bootloader boot slot to %s", slot.c_str());
+      if (slot == "A" || slot == "B") device->GetUI()->Print("Error changing bootloader boot slot to %s", slot.c_str());
     }
   }
   return ret.success ? 0 : 1;
